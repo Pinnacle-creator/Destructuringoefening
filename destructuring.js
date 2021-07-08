cb3e2e8497bba42a39dd27fd3192cf6dfd5ac948
Pinnacle-creator <joerir@gmail.com>
2021-07-08 14:40:04 +0200

    bijna klaar

--- destructuring.js
+++ destructuring.js
@@ -6,6 +6,11 @@ const product = {
   screenType: 'LED',
 }
 
+const amount = product.price;
+const hz = product.refreshRate;
+console.log(amount, hz);
+
+
 // Opdracht 2: Destructure type en brand uit onderstaand object, en log ze daarna in de terminal
 
 const productInformation = {
@@ -17,6 +22,11 @@ const productInformation = {
   screenQuality: 'Ultra HD/4K',
   smartTv: true,
 }
+
+const model = productInformation.general.type;
+const firm = productInformation.general.brand;
+console.log(model, firm);
+
 
 // Opdracht 3: destructure wifi en bluetooth uit onderstaand object, en log ze daarna in de terminal
 
@@ -38,14 +48,26 @@ const tvOptions = {
   },
 };
 
+const wireless = tvOptions.options.connectivity.popular.wifi;
+const blue = tvOptions.options.connectivity.popular.bluetooth;
+console.log(wireless, blue);
+
+
 // Opdracht 4: Destructure beide waardes uit onderstaande array, en log ze daarna in de terminal
 
 const connectivity = ['wifi', 'bluetooth'];
 
+const [first, second] = connectivity;
+console.log(first);
+console.log(second);
 
 // Opdracht 5: Destructure de >eerste< en >derde< waarde uit onderstaande array, en log ze daarna in de terminal
 
 const tvSizes = [41, 43, 46, 56, 65];
+
+const [first1,, third1] = tvSizes
+console.log(first1);
+console.log(third1);
 
 
 // opdracht 6: Destructure name en adress uit de return value van deze functie en log ze daarna in de terminal
@@ -57,8 +79,14 @@ function getCompanyDetails() {
   }
 }
 
+const school = getCompanyDetails();
+console.log(school);
+
 // opdracht 7: Destructure beide zinnetjes uit de return value van de getDetails functie en log ze in de terminal. Let op: de functie verwacht argumenten bij het aanroepen!
 
 function getDetails(name, age) {
   return [`Your name is ${name}`, `You are ${age} years old`];
 }
+
+const sentence = getDetails.name
+console.log(sentence);
